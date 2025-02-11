3e10f87076710c05526e8842d2acc7989bf71d08
Leascloud <fuerza_alienigena2000@hotmail.com>
2025-02-11 13:31:21 -0600

    Update script12.js

--- script12.js
+++ script12.js
@@ -1,9 +1,9 @@
 function verificarContraseña() {
     var contraseña = prompt("Introduce la contraseña:");
-    var correcta = "12345"; // Cambia esto por la contraseña que desees
+    var correcta = ""; // Cambia esto por la contraseña que desees
 
     if (contraseña === correcta) {
-        window.location.href = "https://www.google.com.mx/?hl=es-419"; // URL de la página a la que deseas redirigir
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSduLcvf5xiEUsyUukf-XplcLfkrr5_FbTQnnJx_tzm2zXF2Hw/viewform?usp=dialog"; // URL de la página a la que deseas redirigir
     } else {
         alert("Contraseña incorrecta.");
     }
