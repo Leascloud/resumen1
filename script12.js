254c87ec11d35359c474c2827ede18d209a9b917
Leascloud <fuerza_alienigena2000@hotmail.com>
2025-02-11 13:36:29 -0600

    Update script12.js

--- script12.js
+++ script12.js
@@ -1,6 +1,6 @@
 function verificarContraseña() {
     var contraseña = prompt("Introduce la contraseña:");
-    var correcta = ""; // Cambia esto por la contraseña que desees
+    var correcta = "bubuydudu"; // Cambia esto por la contraseña que desees
 
     if (contraseña === correcta) {
         window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSduLcvf5xiEUsyUukf-XplcLfkrr5_FbTQnnJx_tzm2zXF2Hw/viewform?usp=dialog"; // URL de la página a la que deseas redirigir
